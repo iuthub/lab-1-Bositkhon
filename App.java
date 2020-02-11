055109dcd1a7408c840d2e8e7581ba46c9557e36
Bositkhon <bositkhon.sh@gmail.com>
2020-02-11 15:44:37 +0500

    finish lab assignment

---
+++ App.java
@@ -0,0 +1,31 @@
+/**
+ * Bositkhon Shoumarov
+ * U1810170
+ * Lab1
+ */
+
+class App{
+  public static void main(String[] args)
+  {
+    // BMICalculator.calculate();
+
+    // InvoiceTest.launch();
+
+    // ComissionCalculator.launch();
+
+    // LargestInteger.launch();
+
+    // Palindrome.launch();
+
+    // TrianglePrint.printA();
+
+    // TrianglePrint.printB();
+
+    // TrianglePrint.printC();
+
+    // TrianglePrint.printD();
+
+    PythagoreanTriples.brutforce();
+
+  }
+}
